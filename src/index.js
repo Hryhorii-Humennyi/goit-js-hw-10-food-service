0f45bfdb9b378612a683daa757df1f02f3d15080
Hryhorii-Humennyi <80813832+Hryhorii-Humennyi@users.noreply.github.com>
2021-11-03 20:51:41 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './sass/main.scss';
+import {refs} from './js/refs.js';
+import {rend} from './js/add-list';
+import { changeThemes } from './js/change-themes.js';
---
+++ src/js/add-list.js
@@ -0,0 +1,9 @@
+export
+import cards from "../templates/cards.hbs";
+import arrayCards from "../menu.json";
+import refs from "./refs";
+const { list } = refs;
+
+const readyСards = cards(arrayCards);
+const rendering = list.insertAdjacentHTML('afterbegin', readyСards);
+export {rendering}
